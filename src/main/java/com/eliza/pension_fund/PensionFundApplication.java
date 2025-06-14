78810a116c4535bf16d7a97bc5aa039f3463c2a7
Elizzaz <elisamorillon07@gmail.com>
2025-06-14 10:13:40 +0200

    initial commit

---
+++ src/main/java/com/eliza/pension_fund/PensionFundApplication.java
@@ -0,0 +1,13 @@
+package com.eliza.pension_fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionFundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliza/pension_fund/PensionFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliza.pension_fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
